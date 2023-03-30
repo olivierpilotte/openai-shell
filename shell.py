2254c5b92aeac6c2034fa3e6762c673d1001ada8
Olivier Pilotte <opilotte@wayup.com>
2023-03-30 16:31:48 -0400

    restructuring a little bit, adding bold text for user queries

--- shell.py
+++ shell.py
@@ -10,6 +10,10 @@ MODEL = "gpt-3.5-turbo"
 IMAGE_SIZE = "1024x1024"
 
 
+BOLD = "\033[1m"
+NORMAL = "\033[0m"
+
+
 QUERY_HISTORY = f"{os.path.expanduser('~')}/.openai_query_history"
 try:
     readline.read_history_file(QUERY_HISTORY)
@@ -19,6 +23,8 @@ except FileNotFoundError:
 
 readline.parse_and_bind("tab: complete")
 
+conversation_history = []
+
 CONVERSATION_HISTORY = f"{os.path.expanduser('~')}/.openai_conversation_history"
 if os.path.isfile(CONVERSATION_HISTORY):
     try:
@@ -26,37 +32,27 @@ if os.path.isfile(CONVERSATION_HISTORY):
             conversation_history = json.loads(file.read())
 
     except Exception:
-        conversation_history = []
+        pass
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-while True:
-    query = input("\nUser › ")
+def _query_dalle(query):
+    response = openai.Image.create(
+        prompt=query,
+        n=1,
+        size=IMAGE_SIZE
+    )
+
+    image_url = response["data"][0]["url"]
+    print(f"\nImage: {image_url}")
+
+
+def _query_openai(query):
+    global conversation_history
+
     conversation_history.append({"role": "user", "content": query})
-
-    if len(query) < 1:
-        continue
-
-    readline.write_history_file(QUERY_HISTORY)
-
-    if query == "flush history":
-        conversation_history = []
-        continue
-
-    if query.startswith("image:"):
-        query.replace("image:", "")
-        response = openai.Image.create(
-            prompt=query,
-            n=1,
-            size=IMAGE_SIZE
-        )
-
-        image_url = response["data"][0]["url"]
-        print(f"\nImage: {image_url}")
-
-        continue
 
     try:
         stream = openai.ChatCompletion.create(
@@ -67,7 +63,7 @@ while True:
 
         ai_response = {"role": "system", "content": ""}
 
-        print("\nAI › ", end="")
+        print(f"\n{NORMAL}AI › ", end="")
         for response in stream:
             if not hasattr(response.choices[0].delta, "content"):
                 continue
@@ -86,3 +82,43 @@ while True:
         print(f"\nSystem › There was an issue querying openai: {e}")
 
     print("")
+
+
+def main_loop():
+    global conversation_history
+
+    while True:
+        try:
+            query = input(f"\n{BOLD}User › ")
+
+        except EOFError:
+            break
+
+        if len(query) < 1:
+            continue
+
+        readline.write_history_file(QUERY_HISTORY)
+
+        if query.startswith("image:"):
+            _query_dalle(query.replace("image:", ""))
+            continue
+
+        match query:
+            case "flush history":
+                conversation_history = []
+                continue
+
+            case "history":
+                print(json.dumps(conversation_history, indent=2))
+                continue
+
+            case _:
+                _query_openai(query)
+
+
+if __name__ == "__main__":
+    try:
+        main_loop()
+
+    except KeyboardInterrupt:
+        pass
